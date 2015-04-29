c953b2774ffd815cc2a69189655bede2c9319bf9
johannes <johannesgj@gmail.com>
2015-04-29 11:14:56 +0200

    now using ffmpeg
    sudo chown root:wheel /usr/local/bin/ffmpeg
    solved the issue

--- Modules/create_speech_function.py
+++ Modules/create_speech_function.py
@@ -1,83 +1,59 @@
 
 CREATE OR REPLACE FUNCTION create_speech(text text)
 RETURNS text AS $$
-a = "SELECT s.word, s.air_date, s.seq_number, s.start, s.stop, s.duration, s.relative_path FROM samples s JOIN ( VALUES"
 
-c = ") AS ordered(word, sort_order) ON ordered.word = s.word ORDER BY ordered.sort_order"
-b = text.split()
-for index, _b in enumerate(b):
-    _z = "(\'%s\',\'%d\')" %(_b, (index+1))
-    if index < len(b)-1:
-        _z += ","
-    b[index] = _z
-rows = plpy.execute(a+" ".join(b)+c)
-root = plpy.execute("SELECT * from config_store where key=\'root\'")[0]['value']
-from moviepy.editor import *
 import os
 import base64
 import hashlib
+import os.path
 
+
+a = "SELECT s.word, s.air_date, s.seq_number, s.start, s.stop, s.duration, s.relative_path FROM samples s JOIN ( VALUES"
+
+c = ") AS ordered(word, sort_order) ON ordered.word = s.word ORDER BY ordered.sort_order"
+words = text.replace (" ", " SILENT_SPACE ").upper().split()
+values = []
+for index, _b in enumerate(words):
+    _z = "(\'%s\',\'%d\')" %(_b, (index+1))
+    if index < len(words)-1:
+        _z += ","
+    values += [_z]
+rows = plpy.execute(a+" ".join(values)+c)
+root = plpy.execute("SELECT * from config_store where key=\'root\'")[0]['value']
 m = hashlib.sha256()
 filename = ""
-last_word = None
-last_duration = 0
 lines = []
+target = words.pop(0)
 for row in rows:
+    if not words:
+        break
     word = row['word']
-    duration = row['duration']
-    path = row['relative_path']
+    if not word == target:
+        continue
+    path = root + "/" + row['relative_path']
     m.update(word)
-    path = root + "/" + path
-    if word == last_word:
-        if last_duration < duration:
-            last_duration = duration
-            #lines[-1] = path
-            #lines[-1] = "file \'" + path + "\'\n"
-            #videos[-1] = [VideoFileClip(path)]
-    else:
-        last_duration = 0
-        lines += [path]
-        #lines += ["file \'" + path + "\'\n"]
-        #videos += [VideoFileClip(path)]
+    lines += [path]
+    target = words.pop(0)
 
-    last_word = word 
 sha = m.digest()
 filename = base64.b32encode(sha).strip("=")
 
+txt_filename = root + "/speeches/" + filename + ".txt"
+mov_filename = root + "/speeches/" + filename + ".mp4"
 
-videos = []
-final_video = None
-for f in lines:
-    videos += [VideoFileClip(f)]
-    if len(videos) > 10:
-        if not final_video:
-            final_video = concatenate(videos)
-        else:
-            final_video = concatenate([final_video] + videos)
-        final_video.write_videofile("/tmp/obamatmp.mp4")
-        while videos:
-            v = videos.pop()
-            del v
-        videos = []
-        final_video = VideoFileClip("/tmp/obamatmp.mp4")
-mov_filename = root + "/speeches/" + filename + ".mp4"  
-         
-final_video = concatenate([final_video] + videos)
-final_video.write_videofile(mov_filename)
-while videos:
-    v = videos.pop()
-    del v
-del final_video
+if os.path.isfile(mov_filename):
+    return mov_filename
 
+with open(txt_filename, 'w') as f:
+    for l in lines:
+        f.write('file \'%s\'\n' % l)
 
-txt_filename = root + "/speeches/" + filename + ".txt"  
-
-
-#import subprocess
-#subprocess.call(["/usr/local/bin/ffmpeg", "-f", "-i", txt_filename, "-c", "copy", mov_filename])    
-
-#final_video.write_videofile(filename)
-return mov_filename
+import subprocess
+try:
+    subprocess.check_call(["/usr/local/bin/ffmpeg", "-f", "concat", "-i", txt_filename, "-c", "copy", mov_filename])
+    return mov_filename
+except subprocess.CalledProcessError, e:
+    return "ERROR"
 
 $$ LANGUAGE plpythonu
 
